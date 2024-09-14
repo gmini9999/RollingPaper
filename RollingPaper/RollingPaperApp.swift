27119d3591065d5bae625bfa6fdc36c32a596183
gmini9999 <gmini0320@gmail.com>
2024-09-14 12:43:57 +0900

    Initial Commit

---
+++ RollingPaper/RollingPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingPaperApp.swift
+//  RollingPaper
+//
+//  Created by gmini on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RollingPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
